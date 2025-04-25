38cf920ce9ad593507b8faf28280d000fa4517e2
DESKTOP-2S63EIQ\student <>
2025-04-25 15:21:44 +0900

    それぞれのシーン分けとカラスの動き（確認してない）のプログラム追加

--- RioMakaimura/Assets/script/zombi.cs
+++ RioMakaimura/Assets/script/zombi.cs
@@ -6,16 +6,21 @@ public class enemy_control : MonoBehaviour
 
 
 	Rigidbody2D rb;                 //Rigidbody2Dの格納
-									
+
 	void Start()
-    {
+	{
 		//アタッチされているComponentを取得
 		rb = GetComponent<Rigidbody2D>();
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 		transform.Translate(-e_moveSpeed, 0.0f, 0.0f);
+	}
+
+	void Enemy_Karasu(Collider2D collision)
+	{
+		transform.position = new Vector3(-e_moveSpeed, Mathf.Sin(Time.time) * 2.0f + transform.position.y, transform.position.z);
 	}
 }
