f17d1cba8a08937b036912a635100d0d5ddacd03
DESKTOP-2S63EIQ\student <>
2025-05-09 10:21:10 +0900

    いったんCMです。元通り

--- RioMakaimura/Assets/script/player_control.cs
+++ RioMakaimura/Assets/script/player_control.cs
@@ -3,120 +3,77 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using System.Diagnostics;
 public class player_control : MonoBehaviour
 {
-	//�ϐ��錾
-	public float moveSpeed;															//�ړ����x
-	public float jumpPower;															//�W�����v��
-	public LayerMask Ground;														//�n�ʂ𔻕ʂ���I�u�W�F�N�g���C���[
-	public GameObject bulletPrefab;												//���̃v���n�u
-	public float KnockbackForce;													//�m�b�N�o�b�N
-	public float invincibleTime;                                                        //���G����
-    public int maxBulletsOnScreen = 3;                                          //��ʓ��ɏo��v���C���[�U���̍ő�̐�
-    public float ClimbSpeed = 0.0f;												//�͂����̈ړ��X�s�[�h
+	//変数宣言
+	public float moveSpeed;                                                         //移動速度
+	public float jumpPower;                                                         //ジャンプ力
+	public LayerMask Ground;                                                        //地面を判別するオブジェクトレイヤー
+	public GameObject bulletPrefab;                                             //槍のプレハブ
+	public float AttackRate;                                                            //攻撃感覚
+	public float CoolDown = 2.0f;                                                 //攻撃のクールダウン
+	public float KnockbackForce;                                                    //ノックバック
+	public float invincibleTime;                                                        //無敵時間
 
 
-    public Vector3 StandSize = new Vector3(3.4f, 3.8f, 1f);             //�����Ă鎞�̃T�C�Y
-	public Vector3 CrouchSize = new Vector3(1.7f, 1.9f, 1f);             //���Ⴊ�񂾎��̃T�C�Y
+	public Vector3 StandSize = new Vector3(3.4f, 3.8f, 1f);             //立ってる時のサイズ
+	public Vector3 SquatSize = new Vector3(1.7f, 1.9f, 1f);             //しゃがんだ時のサイズ
 
 	private int HP = 2;                                                                 //HP
-	private int AttackCount;                                                            //�U�����J�E���g����ϐ�
-	private bool IsSquat = false;													//���Ⴊ�ݔ���
-	private bool IsJumping;															//�󒆂ɂ��邩����
-	private bool IsAttacking = true;												//�U���ł��邩����
-	private bool IsInvincible = false;                                              //���G��Ԃ�����
-    private bool IsClimbing = false;												//�͂�����o���Ă��邩
-    private bool IsOnLadder = false;												//�͂����ɐG��Ă��邩
-    private bool IsCrouching = false;												//���Ⴊ�ݒ���
-    private float Moveinput;                                                          //�ړ�����
-	private float InvincibleTimer;													//���G���ԃ^�C�}�[
-	private Vector2 Movedirection = Vector2.zero;							// �ړ��������L�����Ă���
+	private int AttackCount;                                                            //攻撃をカウントする変数
+	private bool IsSquat = false;                                                   //しゃがみ判定
+	private bool IsJumping;                                                         //空中にいるか判定
+	private bool IsAttacking = true;                                                //攻撃できるか判定
+	private bool IsInvincible = false;                                              //無敵状態か判定
+	private float Moveinput;                                                          //移動入力
+	private float LastAttackTime;                                                   //最後に攻撃した時間
+	private float InvincibleTimer;                                                  //無敵時間タイマー
+	private Vector2 Movedirection = Vector2.zero;                           // 移動方向を記憶しておく
 
-
-
-
-	private Rigidbody2D rb;															//Rigidbody2D�̊i�[
-	private BoxCollider2D bc;														//BoxCollider2D�̊i�[��
+	private Rigidbody2D rb;                                                         //Rigidbody2Dの格納
+	private BoxCollider2D bc;                                                       //BoxCollider2Dの格納庫
 	void Start()
 	{
-		//�A�^�b�`����Ă���Component���擾
+		//アタッチされているComponentを取得
 		rb = GetComponent<Rigidbody2D>();
 		bc = GetComponent<BoxCollider2D>();
-		
+		LastAttackTime = -AttackRate;                                           // 最初の発射が即時できるように設定
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		////�㉺�̃L�[�̔���
-        //float vertical = Input.GetAxisRaw("Vertical");
 
-        //�ړ�����
-        if (/*IsGrounded() == true && */ IsSquat == false /*&& IsJumping == false*/)
-        {
-            Move();
-        }
-
-        ////�W�����v����
-        //if (IsGrounded() == true && IsSquat == false)
-        //{
-        //	Jump();
-        //}
-
-        //      // �� �͂�������
-        //      if (IsOnLadder)
-        //      {
-        //          if (Mathf.Abs(vertical) > 0f)
-        //          {
-        //              IsClimbing = true;
-        //          }
-        //      }
-        //      else
-        //      {
-        //          IsClimbing = false;
-        //      }
-
-        //      if (IsClimbing)
-        //      {
-        //          rb.linearVelocity = new Vector2(rb.linearVelocity.x, vertical * ClimbSpeed);
-        //          rb.gravityScale = 0f;
-        //      }
-        //      else
-        //      {
-        //          rb.gravityScale = 1f;
-        //      }
-
-        //      // �� ���Ⴊ�ݏ����i�͂����ɐG��Ă��Ȃ��Ƃ��̂݁j
-        //      if (!IsOnLadder && vertical < 0)
-        //      {
-        //          if (!IsCrouching)
-        //          {
-        //              IsCrouching = true;
-        //              bc.size = CrouchSize; // ����������
-        //          }
-        //      }
-        //      else
-        //      {
-        //          if (IsCrouching)
-        //          {
-        //              IsCrouching = false;
-        //              bc.size = StandSize; // ���ɖ߂�
-        //          }
-        //      }
-
-
-
-        //Z�L�[�������ꂽ��U��
-        if (Input.GetKeyDown(KeyCode.Z))
+		//移動処理
+		if (/*IsGrounded() == true && */ IsSquat == false /*&& IsJumping == false*/)
 		{
-           
-			Attack();
+			Move();
+		}
 
-        }
+		//ジャンプ処理
+		if (IsGrounded() == true && IsSquat == false)
+		{
+			Jump();
+		}
 
-        //���G�^�C�}�[
-        if (IsInvincible==true)
+		//Zキーが押されたら
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			// 発射可能かつ、発射回数が3回以下の場合に発射
+			if (IsAttacking && AttackCount < 2)
+			{
+				Attack();
+			}
+			// 発射回数が3回に達した場合、制限をかける
+			else if (AttackCount >= 1)
+			{
+				StartCoroutine(AttackCoolDown());
+			}
+		}
+
+
+		//無敵タイマー
+		if (IsInvincible == true)
 		{
 			InvincibleTimer -= Time.deltaTime;
 			if (InvincibleTimer <= 0)
@@ -125,218 +82,106 @@ public class player_control : MonoBehaviour
 			}
 		}
 
-		//���S����
-		if (HP==0)
+
+
+
+		//死亡条件
+		if (HP == 0)
 		{
-			//�v���C���[��j��
+			//プレイヤーを破壊
 			Destroy(gameObject);
 
-			//Debug.Log("����wwwwwwwwww");
+			Debug.Log("死ぬwwwwwwwwww");
 
-			//Scene�����Z�b�g����
+			//Sceneをリセットする
 			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 		}
 
 	}
 
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		//EnemyとEnemyBulletに当たったらプレイヤーを破壊する
+		if ((collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyBullet"))
+		{
 
-
-    
-
-
-
-
-    //�֐����FJump()
-    //�p�r�F�W�����v����
-    //�����F�Ȃ�
-    //�߂�l�F�Ȃ�
-    void Jump()
-    {
-        //�W�����v����
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * jumpPower);
-            //�W�����v��Ԃɂ���
-            IsJumping = true;
-
-            //�W�����v���͈ړ����x�𐧌�����
-            Moveinput *= 0.7f;
-        }
-    }
-
-
-    //�֐����FMove()
-    //�p�r�F�ړ�����
-    //�����F�Ȃ�
-    //�߂�l�F�Ȃ�
-    void Move()
-    {
-
-        //�n��ɂ��Ȃ��Ƃ��͓��͂��󂯕t���Ȃ�
-        if (IsGrounded() == true)
-        {
-            Moveinput = Input.GetAxisRaw("Horizontal");
-
-            //�v���C���[�̌�����ύX
-            if (Moveinput != 0)
-            {
-                Movedirection = new Vector2(Moveinput, 0f);
-
-                // �����𔽓]�����鏈��
-                Vector3 scale = transform.localScale;
-                scale.x = Mathf.Abs(scale.x) * Mathf.Sign(Moveinput); // ���Ȃ�}�C�i�X�A�E�Ȃ�v���X
-                transform.localScale = scale;
-            }
-        }
-
-
-        //�v���C���[���ړ�������
-        transform.Translate(Moveinput * moveSpeed, 0.0f, 0.0f);
-
-        if (Moveinput != 0)
-        {
-            Movedirection = new Vector2(Moveinput, 0f);
-        }
-
-
-    }
-
-    //�͂����ɐG��Ă��邩
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.CompareTag("Ladder"))
-    //    {
-    //        IsOnLadder = true;
-    //    }
-    //}
-
-    ////�͂������痣�ꂽ��
-    //private void OnTriggerExit2D(Collider2D other)
-    //{
-    //    if (other.CompareTag("Ladder"))
-    //    {
-    //        IsOnLadder = false;
-    //        IsClimbing = false;
-    //    }
-    //}
-
-
-    //�֐����FAttack()
-    //�p�r�F�U������
-    //�����F�Ȃ�
-    //�߂�l�F�Ȃ�
-    private void Attack()
-    {
-
-
-        //���I�u�W�F�N�g�����ׂĎ擾���邽�߂ɔz����쐬
-        GameObject[] bullets = GameObject.FindGameObjectsWithTag("Spear");
-
-        if (bullets.Length >= maxBulletsOnScreen)
-        {
-            // ��ʂ̍ő吔�ɒB���Ă���̂Ŕ��˂��Ȃ�
-
-            return;
-        }
-
-        // �U������
-        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-
-        // �v���C���[�̌����ɍ��킹�Ĕ��]
-        bullet.transform.localScale = new Vector3(Mathf.Sign(transform.localScale.x), 1, 1);
-
-
-    }
-
-
-    //��ʊO�ɏo���炱�̃I�u�W�F�N�g��j��
-    private void OnBecameInvisible()
-    {
-        Destroy(gameObject);
-
-        //Scene�����Z�b�g����
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+			HP -= 1;
+			Debug.Log("痛い");
 
 
 
-    //�_���[�W����
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //Enemy��EnemyBullet�ɓ���������v���C���[��j�󂷂�
-        if ((collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "EnemyBullet"))
-        {
+			//if (collision.gameObject.CompareTag("Player")&&IsInvincible==true)
+			//{
+			//		// 衝突を無視する
+			//		Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+			//}
 
-            HP -= 1;
-            //Debug.Log("�ɂ�");
+			// ノックバック処理
+			Vector2 knockbackDirection = transform.position.x < collision.transform.position.x ? Vector2.left : Vector2.right;
+			GetComponent<Rigidbody2D>().AddForce(knockbackDirection * KnockbackForce, ForceMode2D.Impulse);
 
-
-
-            // �m�b�N�o�b�N����
-            Vector2 knockbackDirection = transform.position.x < collision.transform.position.x ? Vector2.left : Vector2.right;
-            GetComponent<Rigidbody2D>().AddForce(knockbackDirection * KnockbackForce, ForceMode2D.Impulse);
-
-            // ���G��Ԃ��J�n
-            StartInvincibility();
-        }
-    }
+			// 無敵状態を開始
+			StartInvincibility();
+		}
+	}
 
 
-    //�֐����FStartInvincibility()
-    //�p�r�F���G���ԏ���
-    //�����F�Ȃ�
-    //�߂�l�F�Ȃ�
-    void StartInvincibility()
+
+	//関数名：StartInvincibility()
+	//用途：無敵時間処理
+	//引数：なし
+	//戻り値：なし
+	void StartInvincibility()
 	{
 		IsInvincible = true;
 		InvincibleTimer = invincibleTime;
 
-		// ���G���A�G�Ƃ̏Փ˂𖳎�����
+		// 無敵時、敵との衝突を無視する
 		StartCoroutine(IgnoreEnemyCollisionDuringInvincibility());
 
-		// �v���C���[��_�ł����邽�߂̃R���[�`�����J�n
+		// プレイヤーを点滅させるためのコルーチンを開始
 		StartCoroutine(InvincibilityFlash());
 	}
 
 
 
-	//�֐����FInvincibilityFlash()
-	//�p�r�F�_�ŏ���
-	//�����F�Ȃ�
-	//�߂�l�F�Ȃ�
-	// �v���C���[��_�ł�����R���[�`��
+	//関数名：InvincibilityFlash()
+	//用途：点滅処理
+	//引数：なし
+	//戻り値：なし
+	// プレイヤーを点滅させるコルーチン
 	IEnumerator InvincibilityFlash()
 	{
 		SpriteRenderer sr = GetComponent<SpriteRenderer>();
 		while (IsInvincible)
 		{
-			sr.enabled = !sr.enabled; // �_�Łi�X�v���C�g�̕\���E��\���؂�ւ��j
-			yield return new WaitForSeconds(0.1f); // �_�ł̊Ԋu
+			sr.enabled = !sr.enabled; // 点滅（スプライトの表示・非表示切り替え）
+			yield return new WaitForSeconds(0.1f); // 点滅の間隔
 		}
-		sr.enabled = true; // �Ō�ɃX�v���C�g��\��
+		sr.enabled = true; // 最後にスプライトを表示
 	}
 
 
-	// ���G���Ԓ��A�v���C���[�ƓG�̏Փ˂𖳎�����
+	// 無敵時間中、プレイヤーと敵の衝突を無視する
 	IEnumerator IgnoreEnemyCollisionDuringInvincibility()
 	{
-		// ���G���Ԃ��I������܂ŌJ��Ԃ�
+		// 無敵時間が終了するまで繰り返し
 		while (IsInvincible)
 		{
-			// "Enemy" �^�O�̂����I�u�W�F�N�g�����ׂĎ擾
+			// "Enemy" タグのついたオブジェクトをすべて取得
 			GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
-			// �G�Ƃ̏Փ˂𖳎�
+			// 敵との衝突を無視
 			foreach (GameObject enemy in enemies)
 			{
 				Physics2D.IgnoreCollision(bc, enemy.GetComponent<Collider2D>(), true);
 			}
 
-			// ���G���Ԓ��͏Փ˂𖳎���������
+			// 無敵時間中は衝突を無視し続ける
 			yield return null;
 		}
 
-		// ���G���Ԃ��I��������A�Փ˂��ĂїL���ɂ���
+		// 無敵時間が終了したら、衝突を再び有効にする
 		GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
 		foreach (GameObject enemy in allEnemies)
 		{
@@ -345,21 +190,117 @@ public class player_control : MonoBehaviour
 	}
 
 
-	
+	//関数名：Jump()
+	//用途：ジャンプ処理
+	//引数：なし
+	//戻り値：なし
+	void Jump()
+	{
+		//ジャンプ処理
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb.AddForce(Vector2.up * jumpPower);
+			//ジャンプ状態にする
+			IsJumping = true;
+
+			//ジャンプ中は移動速度を制限する
+			Moveinput *= 0.7f;
+		}
+	}
+
+
+	//関数名：Move()
+	//用途：移動処理
+	//引数：なし
+	//戻り値：なし
+	void Move()
+	{
+
+		//地上にいないときは入力を受け付けない
+		if (IsGrounded() == true)
+		{
+			Moveinput = Input.GetAxisRaw("Horizontal");
+
+			//プレイヤーの向きを変更
+			if (Moveinput != 0)
+			{
+				Movedirection = new Vector2(Moveinput, 0f);
+
+				// 向きを反転させる処理
+				Vector3 scale = transform.localScale;
+				scale.x = Mathf.Abs(scale.x) * Mathf.Sign(Moveinput); // 左ならマイナス、右ならプラス
+				transform.localScale = scale;
+			}
+		}
+
+
+		//プレイヤーを移動させる
+		transform.Translate(Moveinput * moveSpeed, 0.0f, 0.0f);
+
+		if (Moveinput != 0)
+		{
+			Movedirection = new Vector2(Moveinput, 0f);
+		}
+
+
+	}
+
+	//関数名：CoolDown()
+	//用途：攻撃間隔を設ける処理
+	//引数：なし
+	//戻り値：なし
+	private IEnumerator AttackCoolDown()
+	{
+		// 発射制限中の処理
+		IsAttacking = false;
+		Debug.Log("発射制限中...");
+
+		// 制限時間を待つ
+		yield return new WaitForSeconds(CoolDown);
+
+		// 発射制限解除
+		IsAttacking = true;
+		AttackCount = 0; // 発射回数をリセット
+		Debug.Log("発射再開！");
+	}
+
+
+	//関数名：Attack()
+	//用途：攻撃処理
+	//引数：なし
+	//戻り値：なし
+	private void Attack()
+	{
+		// 攻撃処理
+		GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+		bullet.transform.localScale = new Vector3(Mathf.Sign(transform.localScale.x), 1, 1); // プレイヤーの向きに合わせて反転
+
+		// 発射回数をカウント
+		AttackCount++;
+
+		// 最後に発射した時刻を更新
+		LastAttackTime = Time.time;
+
+		// 3回目の発射後に発射制限をかける
+		if (AttackCount >= 2)
+		{
+			StartCoroutine(AttackCoolDown());
+		}
+	}
 
 
 
 
-    //�֐����FIsGrounded()
-    //�p�r�F�ڒn���菈��
-    //�����F�Ȃ�
-    //�߂�l�F�ڒn���Ă���ꍇ��true�A���Ă��Ȃ��ꍇ��false
-    bool IsGrounded()
+	//関数名：IsGrounded()
+	//用途：接地判定処理
+	//引数：なし
+	//戻り値：接地している場合はtrue、していない場合はfalse
+	bool IsGrounded()
 	{
 		bool ret = false;
-		//��������ray���΂��āA�w�肵�����C���[�̃I�u�W�F�N�g�ƐڐG���Ă��邩�ǂ������ʂ���
+		//下方向にrayを飛ばして、指定したレイヤーのオブジェクトと接触しているかどうか判別する
 		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.4f, Ground);
-		//�q�b�g���Ă��Ȃ��ꍇ��null���Ԃ����
+		//ヒットしていない場合はnullが返される
 		if (hit.collider != null)
 		{
 			ret = true;
@@ -372,5 +313,3 @@ public class player_control : MonoBehaviour
 		return ret;
 	}
 }
-
-
