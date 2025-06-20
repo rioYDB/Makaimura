5857ae87c1736061ac5f900b35acee051fb0a4d9
DESKTOP-2S63EIQ\student <>
2025-06-20 16:38:36 +0900

    まだ、通常状態完成してない

--- RioMakaimura/Assets/script/bullet/Bullet_Human.cs
+++ RioMakaimura/Assets/script/bullet/Bullet_Human.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class Bullet_Human : bullet
 {
 
+    public float bounceMoveSpeed = 5.0f;  //移動速度
+    public float bounceHeight = 2.0f;     //バウンドする高さ
+    public float bounceDuration = 1.0f;   //バウンドするまでの時間
 
-
+    private Vector3 StartPos;             //弾が発射された位置
+    private float StartTime;              //弾の生成された時間
+    private float XDirection;             //弾のX方向
     protected override void Start()
     {
        
