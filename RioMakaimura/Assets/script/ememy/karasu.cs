75cda6142de3d2266c02cec7caf940b1cad9a988
DESKTOP-2S63EIQ\student <>
2025-07-24 13:28:38 +0900

    すてーじ

--- RioMakaimura/Assets/script/ememy/karasu.cs
+++ RioMakaimura/Assets/script/ememy/karasu.cs
@@ -2,26 +2,34 @@ using UnityEngine;
 
 public class karasu : MonoBehaviour
 {
-	public float e_moveSpeed;       // 敵の移動速度
+    public float e_moveSpeed = 3.0f;       // 敵の移動速度
+    public float floatHeight = 0.5f;       // 上下のふわふわの高さ
+    public float floatSpeed = 1.0f;        // ふわふわの速さ
 
-	Rigidbody2D rb;                 //Rigidbody2Dの格納
-	Renderer Ren;
+    float startY;                          // 初期Y座標を保存
 
-	void Start()
-	{
-		//アタッチされているComponentを取得
-		rb = GetComponent<Rigidbody2D>();
+    void Start()
+    {
+        startY = transform.position.y;
+    }
 
-	}
+    void Update()
+    {
+        // 移動させたX座標を取得
+        float newX = transform.position.x - e_moveSpeed * Time.deltaTime;
 
-	// Update is called once per frame
-	void Update()
-	{
-		transform.Translate(-e_moveSpeed, 0.0f, 0.0f);      //横に移動
-		transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time, 0.3f), transform.position.z);      //ふわふわ飛んでるように見せる
-	}
-	void OnBecameInvisible()
-	{
-		Destroy(this.gameObject);
-	}
+        // Yはふわふわの分だけ上下に揺れる
+        float offsetY = Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        float newY = startY + offsetY;
+
+        // 新しい位置に更新
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
 }
+
+
