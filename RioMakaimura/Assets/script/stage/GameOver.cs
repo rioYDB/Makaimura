ae01f52832d85b79e5aed55681b67a4ba4c54a35
881PC-16\student <>
2025-12-12 16:35:34 +0900

    mmm

--- RioMakaimura/Assets/script/stage/GameOver.cs
+++ RioMakaimura/Assets/script/stage/GameOver.cs
@@ -5,7 +5,9 @@ public class GameOver : MonoBehaviour
 {
     void Start()
     {
-
+        // ゲームオーバーになったステージ名を取得
+        string failedStageName = StageManager.GetCurrentStage();
+        Debug.Log("Game Over at stage: " + failedStageName);
     }
 
     // Update is called once per frame
