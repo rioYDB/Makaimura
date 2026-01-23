f03d38958b44623b21eb62530cd5e87593f608fa
881PC-13\student <>
2026-01-23 12:01:23 +0900

    olkokokoijihuyhgyfgtfdrdrdsr

--- RioMakaimura/Assets/script/GameManager.cs
+++ RioMakaimura/Assets/script/GameManager.cs
@@ -2,34 +2,48 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public static GameManager Instance;
+    public static GameManager Instance;
 
-	[Header("Boss")]
-	public bool isBossDefeated = false;
+    [Header("Boss")]
+    public bool isBossDefeated = false;
 
-	[Header("Goal")]
-	public GameObject goal;   // ゴールを入れる
+    [Header("Door")]
+    public doorcontroller bossDoor;
 
-	void Awake()
-	{
-		if (Instance == null)
-		{
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
-		else
-		{
-			Destroy(gameObject);
-		}
-	}
+    [Header("Goal")]
+    public GameObject goal;
 
-	public void OnBossDefeated()
-	{
-		isBossDefeated = true;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-		if (goal != null)
-		{
-			goal.SetActive(true);
-		}
-	}
+    public void OnBossDefeated()
+    {
+        if (isBossDefeated) return; // 二重防止
+
+        isBossDefeated = true;
+
+        // ドアを開く
+        if (bossDoor != null)
+        {
+            bossDoor.OpenDoor();
+        }
+
+        // ゴール出現
+        if (goal != null)
+        {
+            goal.SetActive(true);
+        }
+
+        Debug.Log("ボス撃破：ドア＆ゴール解放！");
+    }
 }
